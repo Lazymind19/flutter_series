c90fd82fcf35f849f1083229dafdf8503eb2d280
Lazy Mind <srjkadel@gmail.com>
2023-04-05 22:31:59 +0545

    feat : added widget color animation

--- lib/widget/custom_container_box.dart
+++ lib/widget/custom_container_box.dart
@@ -15,11 +15,42 @@ class CustomContainerBox extends StatefulWidget {
   State<CustomContainerBox> createState() => _CustomContainerBoxState();
 }
 
-class _CustomContainerBoxState extends State<CustomContainerBox> {
+class _CustomContainerBoxState extends State<CustomContainerBox>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Color boxColor;
+  late String boxName;
+
   @override
   void initState() {
     super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
     log("Box name is ${widget.textName}");
+    boxColor = widget.color;
+    boxName = widget.textName;
+  }
+
+  @override
+  void didUpdateWidget(covariant CustomContainerBox oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.color != widget.color) {
+      updateWidgetWithAnimation();
+    }
+  }
+
+  updateWidgetWithAnimation() {
+    final firstColor = boxColor;
+    final Animation<double> animationCurve =
+        CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+    final tween = Tween<double>(begin: 0.0, end: 1.0).animate(animationCurve);
+    tween.addListener(() {
+      setState(() {
+        boxColor = Color.lerp(firstColor, widget.color, tween.value)!;
+      });
+    });
+    animationController.reset();
+    animationController.forward();
   }
 
   @override
@@ -27,7 +58,7 @@ class _CustomContainerBoxState extends State<CustomContainerBox> {
     return Container(
       height: 300,
       width: 300,
-      color: widget.color,
+      color: boxColor,
       child: Center(
         child: Text(widget.textName),
       ),
